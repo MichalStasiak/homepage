74900f7c58a7fe7ed9ef1d4eb9dea7b222286ee5
Michał Stasiak <michal.stasiak@onet.pl>
2021-05-01 16:34:38 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log("Cześć!")
+
+let changeColorButton = document.querySelector(".changeBackground");
+let body = document.querySelector(".body");
+let changeMotiv = document.querySelector(".changeMotiv");
+let themeName = document.querySelector(".themeName");
+
+changeColorButton.addEventListener("click", () => {
+    body.classList.toggle("greyBackground");
+});
+
+changeMotiv.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    themeName.innerText = body.classList.contains("dark") ?
+        "jasny" : "ciemny";
+
+});
